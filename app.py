052a011671e4b016339617bbad878139e2223b58
aneeshcheriank <aneeshcheriank@gmail.com>
2024-11-18 13:30:19 +0000

    updated html file

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import flask
+from flask import render_template
+from random import choices
+
+app = Flask(__name__)
+
+def random_fruit():
+    fruits = ['apple', 'cherry', 'orange']
+    return choices(fruits)
+
+@app.route('/')
+def fruit():
+    my_fruit = random_fruit()
+    return render_template('index.html', title='Random Fruit', fruit=my_fruit)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8000, dubug=True)
