18c3e36bff1ab535cb30189f5d1d757ad96cbc2c
aneeshcheriank <aneeshcheriank@gmail.com>
2024-11-18 13:58:17 +0000

    add test function

--- test_app.py
+++ test_app.py
@@ -2,3 +2,6 @@ from app import random_fruit
 
 def test_random_fruit():
     assert random_fruit(['apple']) == ['apple']
+
+def test_random_fruit_1():
+    assert 'apple' or 'cherry' or 'orange' in random_fruit(['apple', 'cherry', 'orange'])
