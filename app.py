0985175ca95035e603845ac986ccc538d3036cdb
aneeshcheriank <aneeshcheriank@gmail.com>
2024-11-18 13:50:06 +0000

    updated the default fruits type to tuple

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from random import choices
 
 app = Flask(__name__)
 
-Fruits = ['apple', 'cherry', 'orange']
+Fruits = ('apple', 'cherry', 'orange')
 
 def random_fruit(fruits = Fruits):
     return choices(fruits)
